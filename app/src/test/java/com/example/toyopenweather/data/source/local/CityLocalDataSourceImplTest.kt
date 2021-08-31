52fd38e9410f7a6deeb6cd79115e02f4844d0de1
Parkduksung <duksung1234@naver.com>
2021-08-31 15:19:01 +0900

    [feature] Red, CityLocalDataSourceImplTest TestCase 생성.

--- app/src/test/java/com/example/toyopenweather/data/source/local/CityLocalDataSourceImplTest.kt
+++ app/src/test/java/com/example/toyopenweather/data/source/local/CityLocalDataSourceImplTest.kt
@@ -1,5 +1,62 @@
 package com.example.toyopenweather.data.source.local
 
-import org.junit.Assert.*
+import com.example.toyopenweather.data.model.CityItem
+import com.example.toyopenweather.data.model.Coord
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.MatcherAssert
+import org.hamcrest.Matchers
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
 
-class CityLocalDataSourceImplTest
+
+@RunWith(MockitoJUnitRunner::class)
+class CityLocalDataSourceImplTest {
+
+
+    @Mock
+    lateinit var cityLocalDataSourceImpl: CityLocalDataSource
+
+
+    @Test
+    fun checkGetCityListSuccessTest() = runBlocking {
+
+        Mockito.`when`(cityLocalDataSourceImpl.getCityList()).thenReturn(mockCityList)
+
+        MatcherAssert.assertThat(
+            "데이터가 존재하므로 성공.",
+            cityLocalDataSourceImpl.getCityList().isNotEmpty(),
+            Matchers.`is`(true)
+        )
+    }
+
+    @Test
+    fun checkGetCityListFailureTest() = runBlocking {
+
+        Mockito.`when`(cityLocalDataSourceImpl.getCityList()).thenThrow(Throwable())
+
+        MatcherAssert.assertThat(
+            "예외가 발생했기 때문에 실패.",
+            cityLocalDataSourceImpl.getCityList(),
+            Matchers.`is`(Throwable())
+        )
+    }
+
+
+    companion object {
+
+        private val mockCityList = mutableListOf<CityItem>().apply {
+            add(
+                CityItem(
+                    id = 707860,
+                    name = "UA",
+                    country = "UA",
+                    coord = Coord(lat = 44.549999, lon = 34.283333)
+                )
+            )
+        }
+
+    }
+}
