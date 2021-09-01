e09f324bb6bea762300441a88399c1c140bf2ca2
Parkduksung <duksung1234@naver.com>
2021-09-01 17:14:19 +0900

    [feature] green, HomeViewModel 에서 getCurrentWeather 가져오는 TestCode 작성 및 Coverage 100

--- app/src/main/java/com/example/toyopenweather/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/toyopenweather/viewmodel/HomeViewModel.kt
@@ -3,14 +3,19 @@ package com.example.toyopenweather.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.toyopenweather.api.response.WeatherResponse
 import com.example.toyopenweather.data.model.CityItem
 import com.example.toyopenweather.data.repo.CityRepository
+import com.example.toyopenweather.data.repo.WeatherRepository
 import com.example.toyopenweather.util.Result
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class HomeViewModel(private val cityRepository: CityRepository) : ViewModel() {
+class HomeViewModel(
+    private val cityRepository: CityRepository,
+    private val weatherRepository: WeatherRepository
+) : ViewModel() {
 
     private val _viewStateLiveData = MutableLiveData<HomeViewState>()
     val viewStateLiveData: LiveData<HomeViewState> = _viewStateLiveData
@@ -28,9 +33,27 @@ class HomeViewModel(private val cityRepository: CityRepository) : ViewModel() {
         }
     }
 
+    suspend fun getCurrentWeatherById(id: Int) {
+        CoroutineScope(Dispatchers.Main).launch {
+            when (val getCurrentWeatherResult = weatherRepository.getCurrentWeatherById(id = id)) {
+                is Result.Success -> {
+                    _viewStateLiveData.value =
+                        HomeViewState.GetCurrentWeather(getCurrentWeatherResult.value)
+                }
+
+                is Result.Failure -> {
+                    _viewStateLiveData.value = HomeViewState.ErrorGetCurrentWeather
+                }
+            }
+        }
+
+    }
+
     sealed class HomeViewState {
         data class GetCityList(val cityList: List<CityItem>) : HomeViewState()
+        data class GetCurrentWeather(val weatherResponse: WeatherResponse) : HomeViewState()
         object ErrorGetCityList : HomeViewState()
+        object ErrorGetCurrentWeather : HomeViewState()
     }
 
 }
