2036300d9b80f3922012231a5c29b267d28f0029
Parkduksung <duksung1234@naver.com>
2021-09-01 13:44:44 +0900

    [feature] refactor, rule 추가 및 상태값 변화를 알기 위한 observer 생성 및 적용. Coverage 100

--- app/src/test/java/com/example/toyopenweather/view/ui/HomeViewModelTest.kt
+++ app/src/test/java/com/example/toyopenweather/view/ui/HomeViewModelTest.kt
@@ -1,16 +1,19 @@
 package com.example.toyopenweather.view.ui
 
+import android.app.Application
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import com.example.toyopenweather.data.model.CityList
 import com.example.toyopenweather.data.repo.CityRepository
 import com.example.toyopenweather.data.repo.CityRepositoryImpl
-import com.example.toyopenweather.data.source.local.CityListLocalDataSourceImplTest
+import com.example.toyopenweather.data.source.local.CityListLocalDataSourceImplTest.Companion.mockCityList
 import com.example.toyopenweather.util.Result
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.setMain
-import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
@@ -19,10 +22,14 @@ import org.mockito.junit.MockitoJUnitRunner
 
 @RunWith(MockitoJUnitRunner::class)
 class HomeViewModelTest {
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
 
     @Mock
     lateinit var cityRepository: CityRepository
 
+    @Mock
+    lateinit var homeViewStateObserver: Observer<HomeViewModel.HomeViewState>
 
     private lateinit var homeViewModel: HomeViewModel
 
@@ -32,17 +39,21 @@ class HomeViewModelTest {
         Dispatchers.setMain(TestCoroutineDispatcher())
         cityRepository = Mockito.mock(CityRepositoryImpl::class.java)
         homeViewModel = HomeViewModel(cityRepository)
+        homeViewModel.viewStateLiveData.observeForever(homeViewStateObserver)
     }
 
 
     @Test
     fun checkGetCityListSuccessStateTest() = runBlocking {
 
-        val successResult = Result.success(CityListLocalDataSourceImplTest.mockCityList)
+        val successResult = Result.success(mockCityList)
 
         Mockito.`when`(cityRepository.getCityList()).thenReturn(successResult)
 
         homeViewModel.getCityList()
+
+        Mockito.verify(homeViewStateObserver)
+            .onChanged(HomeViewModel.HomeViewState.GetCityList(mockCityList))
 
     }
 
@@ -54,6 +65,9 @@ class HomeViewModelTest {
         Mockito.`when`(cityRepository.getCityList()).thenReturn(failResult)
 
         homeViewModel.getCityList()
+
+        Mockito.verify(homeViewStateObserver)
+            .onChanged(HomeViewModel.HomeViewState.ErrorGetCityList)
 
     }
 
