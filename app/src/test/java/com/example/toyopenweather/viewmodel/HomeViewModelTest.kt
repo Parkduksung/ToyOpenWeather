3d0a3b3bc882b5af24e471414fe1449e4a71dc8d
Parkduksung <duksung1234@naver.com>
2021-09-01 17:08:43 +0900

    [feature] red, HomeViewModel 에서 getCurrentWeather 가져오는 TestCase 및 코드 생성.

--- app/src/test/java/com/example/toyopenweather/viewmodel/HomeViewModelTest.kt
+++ app/src/test/java/com/example/toyopenweather/viewmodel/HomeViewModelTest.kt
@@ -2,10 +2,14 @@ package com.example.toyopenweather.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
+import com.example.toyopenweather.api.response.WeatherResponse
 import com.example.toyopenweather.data.model.CityList
 import com.example.toyopenweather.data.repo.CityRepository
 import com.example.toyopenweather.data.repo.CityRepositoryImpl
+import com.example.toyopenweather.data.repo.WeatherRepository
+import com.example.toyopenweather.data.repo.WeatherRepositoryImpl
 import com.example.toyopenweather.data.source.local.CityListLocalDataSourceImplTest.Companion.mockCityList
+import com.example.toyopenweather.data.source.remote.WeatherRemoteDataSourceImplTest.Companion.mockWeatherResponse
 import com.example.toyopenweather.util.Result
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.runBlocking
@@ -28,6 +32,9 @@ class HomeViewModelTest {
     lateinit var cityRepository: CityRepository
 
     @Mock
+    lateinit var weatherRepository: WeatherRepository
+
+    @Mock
     lateinit var homeViewStateObserver: Observer<HomeViewModel.HomeViewState>
 
     private lateinit var homeViewModel: HomeViewModel
@@ -37,7 +44,8 @@ class HomeViewModelTest {
     fun setUp() {
         Dispatchers.setMain(TestCoroutineDispatcher())
         cityRepository = Mockito.mock(CityRepositoryImpl::class.java)
-        homeViewModel = HomeViewModel(cityRepository)
+        weatherRepository = Mockito.mock(WeatherRepositoryImpl::class.java)
+        homeViewModel = HomeViewModel(cityRepository, weatherRepository)
         homeViewModel.viewStateLiveData.observeForever(homeViewStateObserver)
     }
 
@@ -67,6 +75,37 @@ class HomeViewModelTest {
 
         Mockito.verify(homeViewStateObserver)
             .onChanged(HomeViewModel.HomeViewState.ErrorGetCityList)
+
+    }
+
+    @Test
+    fun checkGetCurrentWeatherByIdSuccessTest() = runBlocking {
+
+        val successResult = Result.success(mockWeatherResponse)
+
+        Mockito.`when`(weatherRepository.getCurrentWeatherById(id = 707860))
+            .thenReturn(successResult)
+
+        homeViewModel.getCurrentWeatherById(id = 707860)
+
+        Mockito.verify(homeViewStateObserver)
+            .onChanged(HomeViewModel.HomeViewState.GetCurrentWeather((successResult as Result.Success).value))
+
+    }
+
+    @Test
+    fun checkGetCurrentWeatherByIdFailTest() = runBlocking {
+
+        val failResult = Result.failure<WeatherResponse>(Throwable())
+
+        Mockito.`when`(weatherRepository.getCurrentWeatherById(id = 707860))
+            .thenReturn(failResult)
+
+        homeViewModel.getCurrentWeatherById(id = 707860)
+
+        Mockito.verify(homeViewStateObserver)
+            .onChanged(HomeViewModel.HomeViewState.ErrorGetCurrentWeather)
+
 
     }
 
