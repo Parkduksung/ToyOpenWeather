3466c1f1a42741fef56bfd8116791bac7f5f595a
Parkduksung <duksung1234@naver.com>
2021-09-01 16:55:45 +0900

    [feature] green, WeatherRepositoryImplTest TestCode 작성 및 Coverage 100

--- app/src/main/java/com/example/toyopenweather/data/repo/WeatherRepository.kt
+++ app/src/main/java/com/example/toyopenweather/data/repo/WeatherRepository.kt
@@ -1,4 +1,8 @@
 package com.example.toyopenweather.data.repo
 
+import com.example.toyopenweather.api.response.WeatherResponse
+import com.example.toyopenweather.util.Result
+
 interface WeatherRepository {
+    suspend fun getCurrentWeatherById(id: Int): Result<WeatherResponse>
 }
--- app/src/main/java/com/example/toyopenweather/data/repo/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/example/toyopenweather/data/repo/WeatherRepositoryImpl.kt
@@ -1,4 +1,16 @@
 package com.example.toyopenweather.data.repo
 
-class WeatherRepositoryImpl : WeatherRepository {
+import com.example.toyopenweather.api.response.WeatherResponse
+import com.example.toyopenweather.data.source.remote.WeatherRemoteDataSource
+import com.example.toyopenweather.util.Result
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class WeatherRepositoryImpl(private val weatherRemoteDataSource: WeatherRemoteDataSource) :
+    WeatherRepository {
+
+    override suspend fun getCurrentWeatherById(id: Int): Result<WeatherResponse> =
+        withContext(Dispatchers.IO) {
+            return@withContext weatherRemoteDataSource.getCurrentWeatherById(id = id)
+        }
 }
