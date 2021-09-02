11ac4b2c8e370eca3d716cd4d885ec16f2027489
Parkduksung <duksung1234@naver.com>
2021-09-02 12:43:44 +0900

    [feature] CityDetailFragment 생성.

---
+++ app/src/androidTest/java/com/example/toyopenweather/view/ui/CityDetailFragmentTest.kt
@@ -0,0 +1,5 @@
+package com.example.toyopenweather.view.ui
+
+import org.junit.Assert.*
+
+class CityDetailFragmentTest
---
+++ app/src/main/java/com/example/toyopenweather/view/ui/CityDetailFragment.kt
@@ -0,0 +1,8 @@
+package com.example.toyopenweather.view.ui
+
+import androidx.fragment.app.Fragment
+import com.example.toyopenweather.R
+
+class CityDetailFragment : Fragment(R.layout.fragment_city_detail) {
+
+}
