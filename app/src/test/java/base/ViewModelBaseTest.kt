d6ed3384ce625afa7c42d7611580d13a75f652c4
Parkduksung <duksung1234@naver.com>
2021-09-03 02:23:08 +0900

    [feature] ViewModelBaseTest 생성.

---
+++ app/src/test/java/base/ViewModelBaseTest.kt
@@ -0,0 +1,11 @@
+package base
+
+import androidx.lifecycle.Observer
+import com.example.toyopenweather.base.ViewState
+import org.mockito.Mock
+
+abstract class ViewModelBaseTest : BaseTest() {
+
+    @Mock
+    lateinit var viewStateObserver: Observer<ViewState>
+}
