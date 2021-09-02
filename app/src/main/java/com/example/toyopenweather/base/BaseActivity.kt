8f5fbde48ecf08a6bdfcb4be90a838c6d23467c3
Parkduksung <duksung1234@naver.com>
2021-09-03 02:04:00 +0900

    [feature] 앱 전체 다크모드 적용x, 회전x

--- app/src/main/java/com/example/toyopenweather/base/BaseActivity.kt
+++ app/src/main/java/com/example/toyopenweather/base/BaseActivity.kt
@@ -1,9 +1,11 @@
 package com.example.toyopenweather.base
 
 import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 
@@ -15,6 +17,9 @@ abstract class BaseActivity<B : ViewDataBinding>(@LayoutRes private val layoutId
     @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         binding = DataBindingUtil.setContentView(this, layoutId)
         binding.lifecycleOwner = this
