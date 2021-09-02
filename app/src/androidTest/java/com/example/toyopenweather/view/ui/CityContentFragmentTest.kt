fac0eb6fd7edf49e8ed902437fd381b6365b8462
Parkduksung <duksung1234@naver.com>
2021-09-02 13:39:40 +0900

    [feature] red, Content 화면 관련 TestCase 생성.

--- app/src/androidTest/java/com/example/toyopenweather/view/ui/CityContentFragmentTest.kt
+++ app/src/androidTest/java/com/example/toyopenweather/view/ui/CityContentFragmentTest.kt
@@ -3,10 +3,10 @@ package com.example.toyopenweather.view.ui
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.contrib.RecyclerViewActions
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.toyopenweather.R
-import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -28,5 +28,39 @@ class CityContentFragmentTest {
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
 
     }
+
+
+    @Test
+    fun should_right_add_all_city_item_when_init_view() {
+        ActivityScenario.launch(HomeActivity::class.java)
+
+        Espresso.onView(ViewMatchers.withId(R.id.rv_city)).perform(
+            RecyclerViewActions.scrollToPosition<CityViewHolder>(
+                CITY_LIST_LAST_POSITION
+            )
+        )
+
+        Espresso.onView(ViewMatchers.withText("Hyram"))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+
+    }
+
+    @Test
+    fun should_show_first_item_city_name_when_init_view() {
+        ActivityScenario.launch(HomeActivity::class.java)
+
+        Espresso.onView(ViewMatchers.withText("Hurzuf"))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun should_not_show_last_item_city_name_when_not_scrolled_down() {
+        ActivityScenario.launch(HomeActivity::class.java)
+
+        Espresso.onView(ViewMatchers.withText("Hyram"))
+            .check(ViewAssertions.doesNotExist())
+
+    }
+
 
 }
