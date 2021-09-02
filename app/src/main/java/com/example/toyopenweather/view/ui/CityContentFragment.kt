eee65b1b438cf9de5c524e62146f359c2eb92b2d
Parkduksung <duksung1234@naver.com>
2021-09-02 12:34:36 +0900

    [feature] refactor, 네이밍 수정.

--- app/src/main/java/com/example/toyopenweather/view/ui/CityContentFragment.kt
+++ app/src/main/java/com/example/toyopenweather/view/ui/CityContentFragment.kt
@@ -3,4 +3,4 @@ package com.example.toyopenweather.view.ui
 import androidx.fragment.app.Fragment
 import com.example.toyopenweather.R
 
-class CityMainFragment : Fragment(R.layout.fragment_city_main)
+class CityContentFragment : Fragment(R.layout.fragment_city_content)
