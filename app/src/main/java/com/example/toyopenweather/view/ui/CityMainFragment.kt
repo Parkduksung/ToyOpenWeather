927b2f5f65ba54ab2f2d4a56ea097b8285580979
Parkduksung <duksung1234@naver.com>
2021-09-02 12:33:03 +0900

    [feature] green, CityMainFragment TestCase 통과.

--- app/src/main/java/com/example/toyopenweather/view/ui/CityMainFragment.kt
+++ app/src/main/java/com/example/toyopenweather/view/ui/CityMainFragment.kt
@@ -1,7 +1,6 @@
 package com.example.toyopenweather.view.ui
 
 import androidx.fragment.app.Fragment
+import com.example.toyopenweather.R
 
-class CityMainFragment : Fragment() {
-
-}
+class CityMainFragment : Fragment(R.layout.fragment_city_main)
