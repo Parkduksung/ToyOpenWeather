70f5137c63cbc3e0f457e5923df6b2288a1db387
Parkduksung <duksung1234@naver.com>
2021-09-03 01:49:28 +0900

    [feature] green, api 정상적으로 동작하게 적용 및 TestCoverage 100

--- app/src/main/java/com/example/toyopenweather/api/WeatherApi.kt
+++ app/src/main/java/com/example/toyopenweather/api/WeatherApi.kt
@@ -2,7 +2,19 @@ package com.example.toyopenweather.api
 
 import com.example.toyopenweather.api.response.WeatherResponse
 import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface WeatherApi {
-    fun getCurrentWeatherById(id: Int): Call<WeatherResponse>
+
+    companion object {
+        const val WEATHER_BY_ID = "data/2.5/weather"
+        const val APP_KEY = "a07cb097858e46d4e01fbb55f39849d6"
+    }
+
+    @GET(WEATHER_BY_ID)
+    fun getCurrentWeatherById(
+        @Query("id") id: Int,
+        @Query("appid") appId: String = APP_KEY
+    ): Call<WeatherResponse>
 }
